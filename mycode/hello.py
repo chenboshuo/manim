63859803f5845d12223322c3800c16fa056b52d4
chenboshuo <chenboshuo.hi@163.com>
2020-02-13 19:18:45 +0800

    :sparkles: Something new. 新的过度语法

--- mycode/hello.py
+++ mycode/hello.py
@@ -9,9 +9,23 @@ class HelloWorld(Scene):
         # Making object
         # 创建了一个叫helloworld的对象，颜色为红色（RED），你也可以直接输入十六进制的颜色代码或其他颜色名称（如YELLOW，BLUE_B等，这个在manimlib.constants的COLOR_MAP可以查到）；
         hello = TextMobject("hello world", color=RED)
+        rectangle = Rectangle(color=BLUE)
+        rectangle.surround(hello)  # 让该矩形包裹 hello
+
+        # 将两个东西合并为VGroup,所有Mobject都可以打包为VGroup, VGroup 可以嵌套
+        group1 = VGroup(hello, rectangle)
+
+        hello_manim = TextMobject("hello Manim", color=BLUE)
+        hello_manim.scale(2.5)
 
         # Position
 
         # Showing object
         self.play(Write(hello))
-        self.wait(10)
+        self.wait(5)
+        self.play(FadeIn(rectangle))
+        self.wait(5)
+        self.play(ApplyMethod(group1.scale, 2.5))
+        self.wait(5)
+        self.play(Transform(hello, hello_manim))
+        self.wait(5)
